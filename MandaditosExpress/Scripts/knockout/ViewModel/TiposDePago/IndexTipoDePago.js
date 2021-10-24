66f6f25a70dd4e96442c6188024ee0524f231312
Melkin Antonio Herrera Mendoza <melkin.herrera@unan.edu.ni>
2021-10-24 00:13:40 -0600

    avances en las operaciones CRUD de los tipos de pago

--- MandaditosExpress/Scripts/knockout/ViewModel/TiposDePago/IndexTipoDePago.js
+++ MandaditosExpress/Scripts/knockout/ViewModel/TiposDePago/IndexTipoDePago.js
@@ -14,12 +14,12 @@ function IndexTipoDePago(TipoDePagoCollection) {
 
         if (event.currentTarget.id == "btn-edit") {
             self.ModalViewModel().ModalHeaderViewModel().ModalTitle("editar la información del tipo de pago").ModalHeaderClass("bg-success");
-            self.ModalViewModel().ModalBodyViewModel().TemplateViewModel({ Name: "Velocidad-modal-template", Data: new TipoDePagoViewModel(ko.toJS(TipoDePago)) });
+            self.ModalViewModel().ModalBodyViewModel().TemplateViewModel({ Name: "tipodepago-modal-template", Data: new TipoDePagoViewModel(ko.toJS(TipoDePago)) });
             self.ModalViewModel().FooterViewModel().ActionName("Editar").UrlAction($(event.currentTarget).attr("href"));
         }
         if (event.currentTarget.id == "btn-del") {
             self.ModalViewModel().ModalHeaderViewModel().ModalTitle("Eliminar el tipo de pago").ModalHeaderClass("bg-danger");
-            self.ModalViewModel().ModalBodyViewModel().TemplateViewModel({ Name: "Velocidad-modal-template", Data: new TipoDePagoViewModel(ko.toJS(TipoDePago)) });
+            self.ModalViewModel().ModalBodyViewModel().TemplateViewModel({ Name: "tipodepago-modal-template", Data: new TipoDePagoViewModel(ko.toJS(TipoDePago)) });
             self.ModalViewModel().FooterViewModel().ActionName("Eliminar").UrlAction($(event.currentTarget).attr("href"));
         }
 
